d93343a39af27df481326cec22c7b20bbd5a2ecd
Erick Marques <erick.marquesc@hotmail.com>
2024-03-02 16:10:16 -0300

    perf: :art: Atualizar os nomes das rotas e acrescentar novas rotas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Dashboard } from './pages/app/dashboard'
-import { SingIn } from './pages/auth/sing-in'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { SignUp } from './pages/auth/sign-up'
+import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
@@ -21,8 +22,12 @@ export const router = createBrowserRouter([
     element: <AuthLayout />,
     children: [
       {
-        path: '/sing-in',
-        element: <SingIn />,
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
