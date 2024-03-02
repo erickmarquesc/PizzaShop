b4679d7db9adbff08ebdc87ee62df4189473a3e9
Erick Marques <erick.marquesc@hotmail.com>
2024-03-02 16:11:35 -0300

    perf: :sparkles: Acrescenta um botão com link para página de signUp

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,6 +3,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -12,19 +13,19 @@ const signInForm = z.object({
 
 type SignInForm = z.infer<typeof signInForm>
 
-export function SingIn() {
+export function SignIn() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
-  const handleSingIn = (data: SignInForm) => {
+  const handleSignIn = (data: SignInForm) => {
     toast.success('Enviamos para seu email um link de acesso.', {
       action: {
         label: 'Reenviar link',
         onClick: () => {
-          handleSingIn(data)
+          handleSignIn(data)
         },
       },
     })
@@ -34,6 +35,9 @@ export function SingIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="secondary" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -44,7 +48,7 @@ export function SingIn() {
             </p>
           </div>
 
-          <form onSubmit={handleSubmit(handleSingIn)} className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label>Seu e-mail</Label>
               <Input id="email" type="email" {...register('email')} />
