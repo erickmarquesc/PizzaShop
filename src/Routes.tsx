5ad6b019a333f8b1d8da27acfe4e6944c39702a5
Erick Marques <erick.marquesc@hotmail.com>
2024-03-03 10:52:58 -0300

    perf: :sparkles: Cria uma nova rota

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,6 +5,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/sign-up'
 import { SignIn } from './pages/auth/sign-in'
+import { Orders } from './pages/app/orders/orders'
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
       },
     ],
   },
