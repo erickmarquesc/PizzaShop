f3657fa80c8584ef18ae8e25ce7d9fe0c319aff1
Erick Marques <erick.marquesc@hotmail.com>
2024-03-04 15:37:10 -0300

    perf: :zap: Add novos componentes de grafico na pagina

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -3,6 +3,8 @@ import { MonthRevenueCard } from './month-revenue-card'
 import { MonthOrdersAmountCard } from './month-orders-amount-card'
 import { DayOrdersAmountCard } from './day-orders-amount-card'
 import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
+import { EvenueChart } from './revenue-chart'
+import { PopularProductsChart } from './popular-products-chart'
 
 export function Dashboard() {
   return (
@@ -16,6 +18,11 @@ export function Dashboard() {
           <MonthOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
           <MonthRevenueCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <EvenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </>
